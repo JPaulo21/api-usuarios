76d914f9ef58e358497c50c17574a1a4c4d30d41
João Paulo <jp.sport17@gmail.com>
2023-09-23 14:11:13 -0300

    bug: ajuste na sintaxe na criação da tabela users

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -2,7 +2,7 @@ create table users (
     id integer not null auto_increment,
     username varchar(100) not null,
     password varchar(255) not null,
-    enable tinyint not null;
+    enable tinyint not null,
 
     primary key(id)
 );
