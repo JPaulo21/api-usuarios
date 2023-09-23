919c73b07d2eb353463967c65d266f964477305f
JPaulo21 <85717992+JPaulo21@users.noreply.github.com>
2023-09-23 09:39:45 -0300

    Revert "Feature/3"

--- src/main/java/com/api/usuarios/repository/UserRepository.java
+++ src/main/java/com/api/usuarios/repository/UserRepository.java
@@ -2,10 +2,6 @@ package com.api.usuarios.repository;
 
 import com.api.usuarios.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends JpaRepository<User, String> {
-
-    UserDetails findByUsername(String username);
-
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
--- src/main/java/com/api/usuarios/service/UserService.java
+++ src/main/java/com/api/usuarios/service/UserService.java
@@ -3,30 +3,18 @@ package com.api.usuarios.service;
 import com.api.usuarios.model.User;
 import com.api.usuarios.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Transactional
-    public User saveUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+    public void saveUser(User user){
+        userRepository.save(user);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
-    }
 }
