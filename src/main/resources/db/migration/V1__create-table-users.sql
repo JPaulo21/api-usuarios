773f10964cfa390747a51d49c58280aba9348651
João Paulo <jp.sport17@gmail.com>
2023-09-23 08:59:23 -0300

    feat: Adicionado script para criação da tabela users

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,8 @@
+create table users (
+    id integer not null auto_increment,
+    username varchar(100) not null,
+    password varchar(255) not null,
+    enable tinyint not null;
+
+    primary key(id)
+);
