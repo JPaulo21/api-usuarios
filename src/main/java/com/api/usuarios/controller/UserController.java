b5fb3331ebf4d987b440c0bddab954bd6bfe68a9
João Paulo <jp.sport17@gmail.com>
2023-09-23 09:30:31 -0300

    feat: implementado o UserDetails na class user e implementado UserDetailsService a classe UserService

--- src/main/java/com/api/usuarios/controller/UserController.java
+++ src/main/java/com/api/usuarios/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.api.usuarios.controller;
 
 import com.api.usuarios.dto.UserDTO;
+import com.api.usuarios.model.User;
 import com.api.usuarios.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +9,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDto, UriComponentsBuilder ucb){
+        User user = userService.saveUser(new User(userDto));
+
+        URI location = ucb
+                .path("users/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 
 }
