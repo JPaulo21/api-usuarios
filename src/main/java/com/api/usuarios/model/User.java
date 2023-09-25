5a9f1fa9c94d27ae4c994a558d8c50fa31aeb99b
João Paulo <jp.sport17@gmail.com>
2023-09-25 20:33:55 -0300

    feat: Mudando GenerationType para identity, Construtor para DTO, Anotation com o nome da coluna para enabled, e I mplementando a interface UserDetails

--- src/main/java/com/api/usuarios/model/User.java
+++ src/main/java/com/api/usuarios/model/User.java
@@ -1,24 +1,72 @@
 package com.api.usuarios.model;
 
+import com.api.usuarios.dto.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "User")
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String username;
-
     private String password;
 
+    @Column(name = "enable")
+    private boolean enabled;
+
+    public User(UserDTO userDTO){
+        this.username = userDTO.username();
+        this.password = userDTO.password();
+        this.enabled = true;
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
 
