15023405c80c4b14e15822c73241cf6daea399af
JJoÃão Paulo <jp.sport17@gmail.com>
2024-01-04 23:02:51 -0300

    feat: Adicionado User, pós reorganização

---
+++ src/main/java/com/api/usuarios/entity/User.java
@@ -0,0 +1,78 @@
+package com.api.usuarios.entity;
+
+import com.api.usuarios.web.dto.UserDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Collection;
+import java.util.List;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+
+    @Column(name = "enable")
+    private boolean enabled;
+
+    @Column(name = "dt_register")
+    private LocalDate registerDate;
+
+    public User(UserDTO userDTO){
+        this.username = userDTO.username();
+        this.password = userDTO.password();
+        this.enabled = true;
+        this.registerDate = LocalDate.now(ZoneId.of("America/Sao_Paulo"));
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
+
