cf0be611dc18ac513a346139e6895df250e8e54a
JJoÃão Paulo <jp.sport17@gmail.com>
2024-01-04 22:38:34 -0300

    organize: Retirado classes para reoreorganização

--- src/main/java/com/api/usuarios/exception/Message.java
+++
@@ -1,7 +0,0 @@
-package com.api.usuarios.exception;
-
-import org.springframework.validation.FieldError;
-
-public record Message(String field, String msg) {
-    public Message(FieldError e){ this(e.getField(), e.getDefaultMessage()); }
-}
