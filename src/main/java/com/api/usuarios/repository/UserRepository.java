b38ea8a25b00ba8ebd5e2583f3f57de43bd4d14b
JJoÃão Paulo <jp.sport17@gmail.com>
2024-01-04 22:48:54 -0300

    feat: Alterado retorno do Usuario para um Optional de usuário

--- src/main/java/com/api/usuarios/repository/UserRepository.java
+++ src/main/java/com/api/usuarios/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.api.usuarios.repository;
 
-import com.api.usuarios.model.User;
+import com.api.usuarios.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
-    UserDetails findByUsername(String username);
+    Optional<UserDetails> findByUsername(String username);
 
 }
